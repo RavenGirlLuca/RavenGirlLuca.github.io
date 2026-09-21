3416613bf303515282e6ccf8dbd9da81ced10c89
RavenGirl <86540834+RavenGirlLuca@users.noreply.github.com>
2026-09-21 15:09:43 -0600

    Update sketch.js

--- 00-codingjs/sketch.js
+++ 00-codingjs/sketch.js
@@ -11,7 +11,7 @@ async function setup() {
 }
 
 function draw() {
-  for(let i = 0; i < 100; i++) {
+  for(let i = 0; i < 10; i++) {
     fill(color(random(255),random(255),random(255)))
     circle(random(width),random(height),random(50));
   }
